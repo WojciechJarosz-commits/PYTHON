abe5c03c00a88d0196aed7539ef0d75a622557be
WojciechJarosz-commits <58322959+WojciechJarosz-commits@users.noreply.github.com>
2022-07-21 19:50:43 +0200

    Delete zad8.py

--- zad8.py
+++
@@ -1,8 +0,0 @@
-txt_file = open('zad8.txt', 'w+')
-txt_file.writelines([str(number)+'\n' for number in range(1, 100)])
-
-txt_file1 = open('zad8.txt', 'r')
-print(txt_file1.read())
-
-txt_file1.close()
-txt_file.close()
