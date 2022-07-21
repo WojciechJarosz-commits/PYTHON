199b979fc355c576a6a50370de3058691d041ee5
WojciechJarosz-commits <58322959+WojciechJarosz-commits@users.noreply.github.com>
2022-07-21 19:50:09 +0200

    Delete zad9.py

--- zad9.py
+++
@@ -1,6 +0,0 @@
-try:
-    with open('zad8.txt', 'r') as in_file, open('zad9.txt','w') as out_file:
-            for line in in_file:
-                out_file.write(line)
-except IOError as ioe:
-    print(ioe)
